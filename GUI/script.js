85702d6b7dbca3ee835ce15219ebc65f78b02c4a
kirchner <kirchner@visionconnect.de>
2024-08-28 13:24:09 +0200

    Updated GUI and Server code: refactored HTML, CSS, and JavaScript files modified Python code for sound playback and volume control added Mutagen library for audio

--- GUI/script.js
+++ GUI/script.js
@@ -1,23 +1,35 @@
-// Initial constants and variables
+// Constants and Variables
+const DARK_MODE_TOGGLE = document.getElementById("darkModeToggle");
 const DESCRIPTION = document.getElementById("desc");
 const VOLUME_SLIDER = document.getElementById("volume");
 const VOLUME_VALUE = document.getElementById("volume-value");
-const PLAY_ICON = document.getElementById("toggle-sound");
-const LOOP_ICON = document.getElementById("loop-icon");
+const PLAY_BUTTON = document.getElementById("play-button");
+const REPEAT_BUTTON = document.getElementById("repeat-button");
+const PLAY_ICON = document.getElementById("play-icon");
+const LOOP_ICON = document.getElementById("repeat-icon");
+const VOLUME_ICON = document.getElementById("volume-icon");
+const SOUND_PROGRESS = document.getElementById("sound-progress");
+const MAX_SOUND_LENGTH = document.getElementById("sound-max-length");
+const CURRENT_SOUND_PROGRESS = document.getElementById(
+  "sound-current-progress"
+);
 
-const DARK_MODE_TOGGLE = document.getElementById("darkModeToggle");
+const BASE_ICON_URL = "/static/bootstrap/icons";
+const REPEAT_ON_URL = `${BASE_ICON_URL}/repeat_on.svg`;
+const REPEAT_OFF_URL = `${BASE_ICON_URL}/repeat_off.svg`;
+const START_SOUND_URL = `${BASE_ICON_URL}/play-fill.svg`;
+const STOP_SOUND_URL = `${BASE_ICON_URL}/stop-fill.svg`;
+const VOLUME_UP_URL = `${BASE_ICON_URL}/volume-up.svg`;
+const VOLUME_MUTE_URL = `${BASE_ICON_URL}/volume-mute.svg`;
 
-const REPEAT_ON_URL = "/static/bootstrap/icons/repeat_on.svg";
-const REPEAT_OFF_URL = "/static/bootstrap/icons/repeat_off.svg";
-const START_SOUND_URL = "/static/bootstrap/icons/play-fill.svg";
-const STOP_SOUND_URL = "/static/bootstrap/icons/stop-fill.svg";
-let REPEAT_ON, REPEAT_OFF, START_SOUND, STOP_SOUND;
-
+let REPEAT_ON, REPEAT_OFF, START_SOUND, STOP_SOUND, VOLUME_UP, VOLUME_MUTE;
 let isPlaying = false;
 let loopEnabled = false;
 let soundsData = [];
+let currentSound = null;
+let soundInterval = null;
 
-// Helper function to fetch SVG content
+// Helper Functions
 async function fetchSVG(url) {
   try {
     const response = await fetch(url);
@@ -28,28 +40,46 @@ async function fetchSVG(url) {
   }
 }
 
-// Fetch SVGs for repeat icons
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.className = "toast show";
+  setTimeout(
+    () => (toast.className = toast.className.replace("show", "")),
+    3000
+  );
+}
+
+function setData() {
+  const formData = new FormData(document.getElementById("play-form"));
+  const data = new URLSearchParams();
+  formData.forEach((value, key) => data.append(key, value));
+  data.append("loop", loopEnabled ? "on" : "off");
+  return data;
+}
+
 async function initializeIcons() {
   REPEAT_ON = await fetchSVG(REPEAT_ON_URL);
   REPEAT_OFF = await fetchSVG(REPEAT_OFF_URL);
   START_SOUND = await fetchSVG(START_SOUND_URL);
   STOP_SOUND = await fetchSVG(STOP_SOUND_URL);
+  VOLUME_UP = await fetchSVG(VOLUME_UP_URL);
+  VOLUME_MUTE = await fetchSVG(VOLUME_MUTE_URL);
   setLoopIcon();
   setPlayIcon();
+  setVolumeIcon();
 }
 
-// Function to update sound options based on selected language and category
+// Update Functions
 async function updateSounds() {
   const language = document.getElementById("language").value;
   const category = document.getElementById("category").value;
-
   try {
     const response = await fetch(
       `/sounds?language=${language}&category=${category}`
     );
     const sounds = await response.json();
     soundsData = sounds;
-
     const soundSelect = document.getElementById("sound");
     soundSelect.innerHTML = sounds
       .map(
@@ -61,17 +91,52 @@ async function updateSounds() {
   }
 }
 
-function initializeDarkMode() {
-  // Load the user's theme preference
-  const darkMode = localStorage.getItem("darkMode");
-  if (darkMode === "enabled") {
-    document.body.classList.add("dark-mode");
-    DARK_MODE_TOGGLE.checked = true;
+function setSoundLength() {
+  const selectedSound = soundsData.find(
+    (sound) => sound.filename === document.getElementById("sound").value
+  );
+  if (selectedSound) {
+    const formattedLength = selectedSound.length.toFixed(2);
+    SOUND_PROGRESS.max = formattedLength;
+    SOUND_PROGRESS.value = 0;
+    MAX_SOUND_LENGTH.textContent = formattedLength;
+    CURRENT_SOUND_PROGRESS.textContent = "0.00";
+  } else {
+    SOUND_PROGRESS.max = 0;
+    MAX_SOUND_LENGTH.textContent = "0.00";
+    CURRENT_SOUND_PROGRESS.textContent = "0.00";
   }
 }
 
-// Event listeners
-// Toggle dark mode
+function updateSoundProgress() {
+  const max = parseInt(SOUND_PROGRESS.max);
+  SOUND_PROGRESS.value = 0;
+  soundInterval = setInterval(() => {
+    if (SOUND_PROGRESS.value < max) {
+      const newValue = parseFloat(SOUND_PROGRESS.value) + 1;
+      SOUND_PROGRESS.value = newValue;
+      CURRENT_SOUND_PROGRESS.textContent = newValue.toFixed(2);
+    } else {
+      if (!isPlaying) {
+        SOUND_PROGRESS.value = SOUND_PROGRESS.max;
+        CURRENT_SOUND_PROGRESS.textContent = SOUND_PROGRESS.max;
+        clearInterval(soundInterval);
+      } else {
+        SOUND_PROGRESS.value = 0;
+        CURRENT_SOUND_PROGRESS.textContent = "0.00";
+      }
+    }
+  }, 1000);
+}
+
+function setDescription() {
+  const selectedSound = soundsData.find(
+    (sound) => sound.filename === document.getElementById("sound").value
+  );
+  DESCRIPTION.innerHTML = selectedSound ? selectedSound.description : "";
+}
+
+// Event Listeners
 DARK_MODE_TOGGLE.addEventListener("change", function () {
   if (DARK_MODE_TOGGLE.checked) {
     document.body.classList.add("dark-mode");
@@ -82,62 +147,64 @@ DARK_MODE_TOGGLE.addEventListener("change", function () {
   }
 });
 
-document.getElementById("language").addEventListener("change", updateSounds);
-document.getElementById("category").addEventListener("change", updateSounds);
+document.getElementById("language").addEventListener("change", changeSelection);
+document.getElementById("category").addEventListener("change", changeSelection);
 
-document.getElementById("sound").addEventListener("change", setDescription);
+document.getElementById("sound").addEventListener("change", function () {
+  setDescription();
+  setSoundLength();
+});
 
-function setDescription() {
-  const selectedSound = soundsData.find(
-    (sound) => sound.filename === document.getElementById("sound").value
-  );
-  DESCRIPTION.innerHTML = selectedSound ? selectedSound.description : "";
+async function changeSelection() {
+  await updateSounds();
+  setDescription();
+  setSoundLength();
 }
 
 VOLUME_SLIDER.addEventListener("input", function () {
-  VOLUME_VALUE.textContent = VOLUME_SLIDER.value;
+  const volumePercentage = parseInt(VOLUME_SLIDER.value * 100);
+  VOLUME_VALUE.textContent = `${volumePercentage}%`;
+});
+
+VOLUME_SLIDER.addEventListener("change", function () {
   updateVolume(VOLUME_SLIDER.value);
 });
 
-PLAY_ICON.addEventListener("click", handlePlaying);
+PLAY_BUTTON.addEventListener("click", handlePlaying);
+REPEAT_BUTTON.addEventListener("click", toggleLoop);
 
-LOOP_ICON.addEventListener("click", toggleLoop);
+// Toggle Functions
+function togglePlaying() {
+  isPlaying = !isPlaying;
+  setPlayIcon();
+}
 
 function toggleLoop() {
   loopEnabled = !loopEnabled;
   setLoopIcon();
 }
 
-function setLoopIcon() {
-  LOOP_ICON.innerHTML = loopEnabled ? REPEAT_ON : REPEAT_OFF;
-}
-
-async function handlePlaying() {
-  togglePlaying();
-  const data = setData();
-  isPlaying ? await handlePlaySound(data) : await handleStopSound();
-}
-
-function togglePlaying() {
-  isPlaying = !isPlaying;
-  setPlayIcon();
-}
 function setPlayIcon() {
   PLAY_ICON.innerHTML = isPlaying ? STOP_SOUND : START_SOUND;
 }
 
-// Function to prepare data for sound play
-function setData() {
-  const formData = new FormData(document.getElementById("play-form"));
-  const data = new URLSearchParams();
-  formData.forEach((value, key) => data.append(key, value));
-  data.append("loop", loopEnabled ? "on" : "off");
-  return data;
+function setLoopIcon() {
+  LOOP_ICON.innerHTML = loopEnabled ? REPEAT_ON : REPEAT_OFF;
 }
 
-// Function to handle playing sound
+function setVolumeIcon() {
+  VOLUME_ICON.innerHTML = VOLUME_SLIDER.value > 0 ? VOLUME_UP : VOLUME_MUTE;
+}
+
+// Sound Control Functions
+async function handlePlaying() {
+  togglePlaying();
+  isPlaying ? await handlePlaySound(setData()) : await handleStopSound();
+}
+
 async function handlePlaySound(data) {
   try {
+    updateSoundProgress();
     const response = await fetch("/play", { method: "POST", body: data });
     const message = await response.text();
     togglePlaying();
@@ -147,11 +214,11 @@ async function handlePlaySound(data) {
   }
 }
 
-// Function to handle stopping sound
 async function handleStopSound() {
   try {
     const response = await fetch("/stop", { method: "POST" });
     const message = await response.text();
+    clearInterval(soundInterval);
     togglePlaying();
     showToast(message);
   } catch (error) {
@@ -159,9 +226,9 @@ async function handleStopSound() {
   }
 }
 
-// Function to update volume
 async function updateVolume(volume) {
   try {
+    setVolumeIcon();
     const response = await fetch("/volume", {
       method: "POST",
       body: new URLSearchParams({ volume: volume }),
@@ -173,22 +240,22 @@ async function updateVolume(volume) {
   }
 }
 
-// Function to show toast messages
-function showToast(message) {
-  const toast = document.getElementById("toast");
-  toast.textContent = message;
-  toast.className = "toast show";
-  setTimeout(
-    () => (toast.className = toast.className.replace("show", "")),
-    3000
-  );
+// Initialization Functions
+function initializeDarkMode() {
+  const darkMode = localStorage.getItem("darkMode");
+  if (darkMode === "enabled") {
+    document.body.classList.add("dark-mode");
+    DARK_MODE_TOGGLE.checked = true;
+  }
 }
 
 async function init() {
   await updateSounds();
-  setDescription();
   await initializeIcons();
-  await initializeDarkMode();
+  initializeDarkMode();
+  setDescription();
+  setSoundLength();
 }
 
+// Initialize the application
 init();
--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,8 @@
 import json
 import os
-import pygame
 from flask import Flask, render_template, request, jsonify, send_from_directory
+import pygame
+from mutagen.oggvorbis import OggVorbis
 
 app = Flask(__name__, template_folder='../GUI', static_folder='../GUI')
 
@@ -39,7 +40,7 @@ def index():
     return render_template('index.html', languages=LANGUAGES, categories=CATEGORIES)
 
 @app.route('/favicon.ico')
-def  favicon():
+def favicon():
     return send_from_directory(os.path.join(current_file_dir, 'static'), 'favicon.ico')
 
 
@@ -72,17 +73,39 @@ def play_sound(file: str, loop: bool):
     global stop_playback, current_sound
     pygame.mixer.init()
     current_sound = pygame.mixer.Sound(file)
-    current_sound.set_volume(current_volume)
-
-    while not stop_playback:
-        current_sound.play(loops=-1 if loop else 0)
-        while pygame.mixer.get_busy() and not stop_playback:
-            pygame.time.Clock().tick(10)
-        if not loop:
-            break
     
-    current_sound = None
-    pygame.mixer.quit()
+    # Set volume to 0 for the fade-in effect
+    current_sound.set_volume(0)
+    fade_in_time = 1000  # Fade-in time in milliseconds (1 second)
+    
+    current_sound.play(loops=-1 if loop else 0, fade_ms=fade_in_time)
+    
+    # Gradually increase the volume to the current volume
+    start_ticks = pygame.time.get_ticks()
+    while pygame.time.get_ticks() - start_ticks < fade_in_time:
+        if stop_playback:
+            break  # Exit the loop if playback is stopped
+        
+        elapsed_time = pygame.time.get_ticks() - start_ticks
+        volume = (elapsed_time / fade_in_time) * current_volume
+        current_sound.set_volume(volume)
+        pygame.time.Clock().tick(10)
+
+    # If stop_playback was not triggered, set the volume to the desired level
+    if not stop_playback:
+        current_sound.set_volume(current_volume)
+
+    # Continue playing the sound or stop if requested
+    while pygame.mixer.get_busy() and not stop_playback:
+        pygame.time.Clock().tick(10)
+    
+    if stop_playback:
+        current_sound.stop()  # Ensure the sound stops immediately when requested
+        stop_playback = False  # Reset the flag for future use
+
+    if not loop:
+        pygame.mixer.quit()
+        current_sound = None
 
 @app.route('/stop', methods=['POST'])
 def stop_playback_request():
@@ -96,7 +119,7 @@ def set_volume():
     volume = request.form.get('volume', type=float, default=1.0)
     current_volume = volume
     
-    if  current_sound:
+    if current_sound:
         current_sound.set_volume(current_volume)
 
     return f'Volume set to {volume}', 200
@@ -118,21 +141,26 @@ def get_sounds():
     if not os.path.exists(sounds_path):
         return jsonify({'error': 'Sounds directory not found'}), 404
 
-    sound_files = [os.path.splitext(sound)[0] for sound in os.listdir(sounds_path) if sound.endswith('.ogg')]
-    
     sound_data = []
-    for sound in sound_files:
-        metadata = METADATA.get(sound, {})
-        title = metadata.get('title', '')
-        description = metadata.get('description', '')
-        
-        sound_data.append({
-            'filename': sound,
-            'title': title if title else 'Unknown Title',
-            'description': description if description else 'No Description'
-        })
+    for sound_file in os.listdir(sounds_path):
+        if sound_file.endswith('.ogg'):
+            sound_name = os.path.splitext(sound_file)[0]
+            sound_file_path = os.path.join(sounds_path, sound_file)
+            audio = OggVorbis(sound_file_path)  # Load the audio file
+            sound_length = audio.info.length  # Get the length in seconds
+            
+            metadata = METADATA.get(sound_name, {})
+            title = metadata.get('title', '')
+            description = metadata.get('description', '')
+            
+            sound_data.append({
+                'filename': sound_name,
+                'title': title if title else 'Unknown Title',
+                'description': description if description else 'No Description',
+                'length': sound_length  # Add the length property
+            })
 
     return jsonify(sound_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='192.168.2.128')
